a4af54123bd671d6e534234501955f1e3ca4f2ed
Bridgerto <2740821@students.wits.ac.za>
2025-11-02 20:01:30 +0200

    Setting up Phase 2

---
+++ main.js
@@ -0,0 +1,24 @@
+document.getElementById("nav-bar").innerHTML = 
+` 
+  <div class="toggle-container">  
+      <label class="switch">
+        <input type="checkbox" id="mode-toggle">
+        <span class="slider round"></span>
+      </label>
+    </div>
+
+    <div class="logo">
+      <a href="/index.html">LUMÉA</a>
+    </div>
+
+
+    <ul class="nav-links">
+      <li><a href="/HTML FILES/Cart.html">Cart</a></li>
+      <li class="option-search" >Search</li>
+      <li><a href="/HTML FILES/Account.html">Account</a></li>
+      <li class="option-menu" >Menu</li>
+      <li><a href="/HTML FILES/Wishlist.html">Wishlist</a></li>
+    </ul>
+`;
+
+
