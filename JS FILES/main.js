53db9fb27ee9403feb4797951f6699b6556767c4
Bridgerto <2740821@students.wits.ac.za>
2025-11-07 21:48:31 +0200

    Trying to fix wishlist nav bar again

--- JS FILES/main.js
+++ JS FILES/main.js
@@ -24,6 +24,33 @@ document.getElementById("nav-bar").innerHTML =
     </ul>
 `;
 
+// this is the nav-bar for all the pages in the HTML FILES folder
+document.getElementById("nav-bar2").innerHTML = 
+` 
+  <div class="toggle-container">  
+      <label class="switch">
+        <input type="checkbox" id="mode-toggle">
+        <span class="slider-round">
+          <span class="mode-text" id="mode-text">LUMÉA: BODY</span>
+        </span>
+      </label>
+    </div>
+
+    <div class="logo">
+      <a href="./index.html">LUMÉA</a>
+    </div>
+
+
+    <ul class="nav-links">
+      <li><a href="../index.html"  class="">Home</a></li>
+      <li><a href="./Cart.html" class="Cart"> Cart </a></li>
+      <li class="option-search"  class="Search">Search</li>
+      <li><a href="./Account.html"  class="Account">Account</a></li>
+      <li class="option-menu"  class="Menu">Menu</li>
+      <li><a href="./Wishlist.html"  class="Wishlist">Wishlist</a></li>
+    </ul>
+`;
+
 //MENU OVERLAY
 document.addEventListener("DOMContentLoaded", () => 
 {
