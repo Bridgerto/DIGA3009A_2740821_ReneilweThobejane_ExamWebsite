209188634bc7ac157e1b50fafbf1b14a602c8635
Bridgerto <2740821@students.wits.ac.za>
2025-11-07 22:36:10 +0200

    Again trying to fix the pathway

--- JS FILES/main.js
+++ JS FILES/main.js
@@ -15,16 +15,16 @@ document.getElementById("nav-bar").innerHTML =
 
 
     <ul class="nav-links">
-      <li><a href="./index.html"  class="">Home</a></li>
-      <li><a href="./HTML FILES/Cart.html" class="Cart"> Cart </a></li>
+      <li><a href="/DIGA3009A_2740821_ReneilweThobejane_ExamWebsite/index.html"  class="">Home</a></li>
+      <li><a href="/DIGA3009A_2740821_ReneilweThobejane_ExamWebsite/HTML FILES/Cart.html" class="Cart"> Cart </a></li>
       <li class="option-search"  class="Search">Search</li>
-      <li><a href="./HTML FILES/Account.html"  class="Account">Account</a></li>
+      <li><a href="/DIGA3009A_2740821_ReneilweThobejane_ExamWebsite/HTML FILES/Account.html"  class="Account">Account</a></li>
       <li class="option-menu"  class="Menu">Menu</li>
-      <li><a href="./HTML FILES/Wishlist.html"  class="Wishlist">Wishlist</a></li>
+      <li><a href="/DIGA3009A_2740821_ReneilweThobejane_ExamWebsite/HTML FILES/Wishlist.html"  class="Wishlist">Wishlist</a></li>
     </ul>
 `;
 
-// this is the nav-bar for all the pages in the HTML FILES folder
+/* this is the nav-bar for all the pages in the HTML FILES folder
 document.getElementById("nav-bar2").innerHTML = 
 ` 
   <div class="toggle-container">  
@@ -49,7 +49,7 @@ document.getElementById("nav-bar2").innerHTML =
       <li class="option-menu"  class="Menu">Menu</li>
       <li><a href="./Wishlist.html"  class="Wishlist">Wishlist</a></li>
     </ul>
-`;
+`;*/
 
 //MENU OVERLAY
 document.addEventListener("DOMContentLoaded", () => 
